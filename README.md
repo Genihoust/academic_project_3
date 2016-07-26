d4a371335e9a4835c689313a0d3fa83b3a90c3d2
Najib Arbaoui <arbaouinajib@hotmail.com>
2016-07-25 21:53:12 -0600

    Upload source code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# academic_project3
+# Academic Project 3
+ 	- Restaurant System software
+	- Web server on ZedBoard (Zynq 7000)
+	- Client side on Android
+
+
+
+# Licence :
+
+Only my code is MIT Licence the libraries I used aren't.
---
+++ server_side_c98_vhdl_verilog/burgerqueen.sdk/burgerqueen_sw/module_LWIP/lwIP_webserver.h
@@ -0,0 +1,15 @@
+/*
+ * lwIP_webserver.h
+ *
+ *  Created on: Sep 22, 2015
+ *      Author: Najib Arbaoui
+ */
+
+#ifndef LWIP_WEBSERVER_H_
+#define LWIP_WEBSERVER_H_
+
+//#define RCV_BUCKET_SIZE 30000
+
+void init_netif();
+
+#endif /* LWIP_WEBSERVER_H_ */
